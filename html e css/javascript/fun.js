78dfb069dab4171837fd787368313807f79a46fa
nataliapaulino <natalia_paulinobatista@hotmail.com>
2021-03-10 13:57:31 -0300

    js

---
+++ html e css/javascript/fun.js
@@ -0,0 +1,18 @@
+palindromo("aba")
+palindromo("rada")
+palindromo("xyzzyx")
+palindromo("bola")
+palindromo("abcddcb")
+palindromo("")
+palindromo("x")
+function palindromo(s){
+    //implementar
+    if(s.length <=1){
+        console.log(s+'é palindromo' );´
+  
+    
+    else{
+        console.log(s+'é indefinido' );
+    }
+
+}
