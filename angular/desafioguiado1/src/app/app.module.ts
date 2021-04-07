1579505efe1c2f1324e41acee7363d9ddb292678
nataliapaulino <natalia_paulinobatista@hotmail.com>
2021-04-07 14:33:50 -0300

    primero erro

---
+++ angular/desafioguiado1/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TictactoeComponent } from './tictactoe/tictactoe.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TictactoeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
