66dc5896a9fce6d61f93acdb89961e7980e97678
nataliapaulino <natalia_paulinobatista@hotmail.com>
2021-04-07 11:32:31 -0300

    igti

--- angular/minha-app/src/app/app.module.ts
+++ angular/minha-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { C1Component } from './c1.component';
+import { C2Component } from './c2/c2.component';
 
 @NgModule({
   declarations: [
     AppComponent
-    C1Component
+    C1Component,
+    C2Component
   ],
   imports: [
     BrowserModule
---
+++ angular/minha-app/src/app/c2/c2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-c2',
+  templateUrl: 'Este é o meu template',
+  styleUrls: ['./c2.component.css']
+})
+export class C2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
